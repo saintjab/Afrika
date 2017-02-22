3424d94d3a1df5bf7418a9cab6a2b673d3794f26
Jonas Atta Boateng <jaboateng@ymail.com>
2017-02-22 13:38:54 +0000

    Added
    Afrika

---
+++ story_content/user.js
@@ -0,0 +1,295 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lpM2cwgmIR":
+        Script1();
+        break;
+      case "6GohCqL9wLD":
+        Script2();
+        break;
+      case "6fmgqsuVDhx":
+        Script3();
+        break;
+      case "5WCAdY5VRkI":
+        Script4();
+        break;
+      case "60crwU3gAsD":
+        Script5();
+        break;
+      case "5VhW7R21SDS":
+        Script6();
+        break;
+      case "6MTz5JchABh":
+        Script7();
+        break;
+      case "5llJltu3Fjo":
+        Script8();
+        break;
+      case "5ZCG3EnnbYk":
+        Script9();
+        break;
+      case "5c5NW1QEdUO":
+        Script10();
+        break;
+      case "6Jb0Rtxl09n":
+        Script11();
+        break;
+      case "6JB9qvnbb8K":
+        Script12();
+        break;
+      case "6c4b869Is9d":
+        Script13();
+        break;
+      case "6cLnhvEVgRt":
+        Script14();
+        break;
+      case "6l20A49TdUc":
+        Script15();
+        break;
+      case "65Tlp5rrJJg":
+        Script16();
+        break;
+      case "5gTFE0NPWVn":
+        Script17();
+        break;
+      case "6qvGxedWdOs":
+        Script18();
+        break;
+      case "6llGEOxd42L":
+        Script19();
+        break;
+      case "6AV40RSIo0k":
+        Script20();
+        break;
+      case "5dfkJJyfu2j":
+        Script21();
+        break;
+      case "6FGwX07Uarg":
+        Script22();
+        break;
+      case "5sdKRUp6ovI":
+        Script23();
+        break;
+      case "5bIM5D2pAqI":
+        Script24();
+        break;
+      case "6k8dyXXY7DY":
+        Script25();
+        break;
+      case "6QQIzKWeqbw":
+        Script26();
+        break;
+      case "5xa0CAYSzsc":
+        Script27();
+        break;
+      case "5ez18jcn546":
+        Script28();
+        break;
+      case "5yWOKMQTPhx":
+        Script29();
+        break;
+      case "5kFeLW4sZyi":
+        Script30();
+        break;
+      case "6nPn8CNbhOm":
+        Script31();
+        break;
+      case "5Yu300EOi2V":
+        Script32();
+        break;
+  }
+}
+
+function Script1()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script2()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script3()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script4()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script5()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script6()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script7()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script8()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script9()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script10()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script11()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script12()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script13()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script14()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script15()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script16()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script17()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script18()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script19()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script20()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script21()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script22()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script23()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script24()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script25()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script26()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script27()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script28()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script29()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script30()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
+function Script31()
+{
+  var volume_level=1
+change_volume(volume_level)
+}
+
+function Script32()
+{
+  var volume_level=0
+change_volume(volume_level)
+}
+
